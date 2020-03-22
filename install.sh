723101400a0df395a232fc841c0473e8f6bc5b8b
Caden <cadenmiller65@yahoo.com>
2020-03-22 15:16:21 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+mkdir /usr/share/tuxman/
 cp Tuxman.desktop /usr/share/applications/
 cp tuxman /usr/bin/
-cp -r ../tuxman/ /usr/share/
-
-rm /usr/share/tuxman/tuxman
-rm /usr/share/tuxman/Tuxman.desktop
-rm /usr/share/tuxman/Makefile
-rm /usr/share/tuxman/install.sh
-rm /usr/share/tuxman/tuxman_logo.xcf
+cp -r data/ /usr/share/tuxman/
+cp -r graphics/ /usr/share/tuxman/
+cp -r include/ /usr/share/tuxman/
+cp -r src/ /usr/share/tuxman/
+cp README.md /usr/share/tuxman/
+cp LICENSE.md /usr/share/tuxman/
+cp tuxman_logo.png /usr/share/tuxman/
