af5d998552b5e48cd9810e329ca7761775f5681d
Caden <cadenmiller65@yahoo.com>
2020-03-22 14:47:45 -0500

    install script now deletes unneccessary items from /usr/share/Tuxman

--- install.sh
+++ install.sh
@@ -3,4 +3,9 @@
 cp Tuxman.desktop /usr/share/applications/
 cp Tuxman /usr/bin/
 cp -r ../Tuxman/ /usr/share/
+
 rm /usr/share/Tuxman/Tuxman
+rm /usr/share/Tuxman/Tuxman.desktop
+rm /usr/share/Tuxman/Makefile
+rm /usr/share/Tuxman/install.sh
+rm /usr/share/Tuxman/Tuxman_logo.xcf
